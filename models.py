07d2cdbbbf9fe474eaa58467f07a11504ac318cd
gabriele.palaj <gabriele.palaj@logaut.it>
2024-07-14 21:38:55 +0200

    Modifica modelli
    Aggiunta tabella differernze

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
-
 from . import db
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,10 +9,12 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
 
+
 class Website(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String(200), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
 
 class MonitoredArea(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,14 +23,14 @@ class MonitoredArea(db.Model):
     name = db.Column(db.String(100), nullable=False)
     area_selector = db.Column(db.String(500), nullable=False)
     time_interval = db.Column(db.Integer, nullable=False, default=60)
-    last_change_checked = db.Column(db.DateTime)
+    last_change_checked = db.Column(db.DateTime, nullable=True)
 
     website = db.relationship('Website', backref=db.backref('monitored_areas', lazy=True))
+    changes = db.relationship('Change', back_populates='monitored_area', cascade="all, delete-orphan")
 
     def __init__(self, **kwargs):
         super(MonitoredArea, self).__init__(**kwargs)
-        if not self.last_change_checked:
-            self.last_change_checked = datetime.now() - timedelta(minutes=self.time_interval)
+        self.last_change_checked = None  # Explicitly set to None for new instances
 
 
 class Change(db.Model):
@@ -39,3 +41,20 @@ class Change(db.Model):
     change_summary = db.Column(db.Text)
     screenshot = db.Column(db.LargeBinary, nullable=False)
     reviewed = db.Column(db.Boolean, default=False)
+
+    monitored_area = db.relationship('MonitoredArea', back_populates='changes')
+    differences1 = db.relationship('Difference', foreign_keys='Difference.change_id1', back_populates='change1',
+                                   cascade="all, delete-orphan")
+    differences2 = db.relationship('Difference', foreign_keys='Difference.change_id2', back_populates='change2',
+                                   cascade="all, delete-orphan")
+
+
+class Difference(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    change_id1 = db.Column(db.Integer, db.ForeignKey('change.id'), nullable=False)
+    change_id2 = db.Column(db.Integer, db.ForeignKey('change.id'), nullable=False)
+    diff_image = db.Column(db.LargeBinary, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    change1 = db.relationship('Change', foreign_keys=[change_id1], back_populates='differences1')
+    change2 = db.relationship('Change', foreign_keys=[change_id2], back_populates='differences2')
